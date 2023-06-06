663932bb14d47da9f6292868ad69a888b8121084
Kishore <kishorekumarsaran2001@gmail.com>
2023-06-06 12:04:43 +0000

    homtemp card and aboutme content added iin website

---
+++ templates/aboutme.php
@@ -0,0 +1,16 @@
+<div class="row content">
+	<div class="col-lg-7 abt" style="background-color:rgb(237,237,237);">
+		<h1>About Me</h1>
+		<h1 style="font-size: 35px; font-weight: 555;">Kishore Kumar</h1>
+		<p style="font-family: ProximaNova; font-size: 20px; line-height: 1.56;">
+			Started programming at the age of 15, I got a keen interest towards Cyber Security and have explored
+			multiple dimensions of it. I took the road of Entrepreneurship and progressively
+			worked with multiple companies and startups on multiple technologies ranging from software to
+			security to hardware, which gave me a brief exposure towards the industry. You can contact me for a
+			coffee or to get your idea into code or to consult for your software, security or hardware needs.
+		</p>
+	</div>
+	<div class="col-lg-5 abtimg">
+		<img src="/images/bg1.jpg" alt="" height="500px" width="500px">
+	</div>
+</div>
---
+++ templates/card.php
@@ -0,0 +1,43 @@
+<div class="container" style="margin-bottom: 100px;">
+	<div class="container">
+		<h1
+			style="color:white;font-family: Avenir;font-size: 50px;font-weight: 900;text-align:center;margin-bottom:100px;">
+			Services
+		</h1>
+	</div>
+	<div class="row">
+		<div class="col-lg-4">
+			<div class="card" style="width: 18rem; color:white; background-color:#202121;">
+				<img class="card-img-top" src="/images/card1.png" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title" style="color: white; border-bottom: 1px solid white;font-weight: 1000;	">
+						Service 1</h5>
+					<p class="card-text">Fullstack Website Development both frontend and backend</p><br>
+					<a href="#" class="hmbtn cardbtn">Visit Service</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-4">
+			<div class="card" style="width: 18rem; color:white; background-color:#202121;">
+				<img class="card-img-top" src="/images/card2.png" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title" style="color: white; border-bottom: 1px solid white;font-weight: 1000;">
+						Service 2</h5>
+					<p class="card-text">Mobile Application<br> Development</p><br>
+					<a href="#" class="hmbtn cardbtn">Visit Service</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-4">
+			<div class="card" style="width: 18rem; color:white; background-color:#202121;">
+				<img class="card-img-top" src="/images/card3.png" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title" style="color: white; border-bottom: 1px solid white;font-weight: 1000;">
+						Service 3</h5>
+					<p class="card-text">Hardware or IOT Development</p><br><br>
+					<a href="#" class="hmbtn cardbtn">Visit Service</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
