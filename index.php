66b35cd75cf71e9919c2661c5cfd23fcfa95dc2d
Kishore <kishorekumarsaran2001@gmail.com>
2023-06-06 03:10:27 +0000

    Jumbotron updated

--- index.php
+++ index.php
@@ -1,25 +1,99 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>CSTECHWORK</title>
-    <?php include_once "templates/headlink.php" ?>
 
-  </head>
-  <body>
-  
-  <?php include_once "templates/navbar.php" ?>
+<head>
+	<title>CSTECHWORK</title>
+	<?php include_once "templates/headlink.php" ?>
 
-  
-  <div class="divbg " style="background-image: url(/images/bg.jpg);"></div>
-  <div class="opacityedit"></div>
-  
-  <div class="hedit">
-    <h1>Welcome to CSTECHWORK</h1><br>
-    <h2>A Software Engineer. I Help Startups Launch And Grow Their Products</h2><br>
-    <a href="#" class="hmbtn"><i class="fa-solid fa-laptop-code"></i>  Services</a>
-  </div>
+</head>
 
-  <?php include_once "templates/b4jq.php" ?>
-  
-  </body>
-</html>     
+<body>
+
+	<?php include_once "templates/navbar.php" ?>
+
+
+	<div class="divbg " style="background-image: url(/images/bg.jpg);"></div>
+	<div class="opacityedit"></div>
+
+	<div class="jumbotron hedit">
+		<h1>Welcome to CSTECHWORK</h1><br>
+		<h2>A Software Engineer. I Help Startups Launch And Grow Their Products</h2><br>
+		<a href="#" class="hmbtn"><i class="fa-solid fa-laptop-code"></i> Services</a>
+	</div>
+
+
+	<div class="container">
+		<table class="table">
+			<thead>
+				<tr>
+					<th scope="col">#</th>
+					<th scope="col">First</th>
+					<th scope="col">Last</th>
+					<th scope="col">Handle</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th scope="row">1</th>
+					<td>Mark</td>
+					<td>Otto</td>
+					<td>@mdo</td>
+				</tr>
+				<tr>
+					<th scope="row">2</th>
+					<td>Jacob</td>
+					<td>Thornton</td>
+					<td>@fat</td>
+				</tr>
+				<tr>
+					<th scope="row">3</th>
+					<td>Larry</td>
+					<td>the Bird</td>
+					<td>@twitter</td>
+				</tr>
+				<tr>
+					<th scope="row">1</th>
+					<td>Mark</td>
+					<td>Otto</td>
+					<td>@mdo</td>
+				</tr>
+				<tr>
+					<th scope="row">2</th>
+					<td>Jacob</td>
+					<td>Thornton</td>
+					<td>@fat</td>
+				</tr>
+				<tr>
+					<th scope="row">3</th>
+					<td>Larry</td>
+					<td>the Bird</td>
+					<td>@twitter</td>
+				</tr>
+				<tr>
+					<th scope="row">1</th>
+					<td>Mark</td>
+					<td>Otto</td>
+					<td>@mdo</td>
+				</tr>
+				<tr>
+					<th scope="row">2</th>
+					<td>Jacob</td>
+					<td>Thornton</td>
+					<td>@fat</td>
+				</tr>
+				<tr>
+					<th scope="row">3</th>
+					<td>Larry</td>
+					<td>the Bird</td>
+					<td>@twitter</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+
+	<?php include_once "templates/b4jq.php" ?>
+
+</body>
+
+</html>
